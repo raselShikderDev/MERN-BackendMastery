054f486460129679eb62dc05bb987512da7cd489
Rasel Shikder <rasel.sikder777.rk@gmail.com>
2025-06-15 11:51:14 +0600

    Added module-17 - 1.0

--- module-17/src/app/app.ts
+++ module-17/src/app/app.ts
@@ -1,9 +1,51 @@
+
 import express, { Application, Request, Response } from "express";
+import myNote from "./noteapp/note";
+import mongoose, { Schema } from "mongoose";
 
-const app:Application = express()
+const app: Application = express()
 
-app.get("/", (req: Request, res:Response)=>{
+app.use(express.json())
+app.use("/mynote", myNote)
+
+const notesSchema = new Schema({
+    title: String,
+    body: String,
+    isActive: Boolean,
+    numberOfChar: Number,
+    date: { type: Date, default: Date.now },
+    comments: [
+        { title: String, body: String }
+    ]
+})
+
+const noteModel = mongoose.model("Note", notesSchema)
+
+app.get("/", (req: Request, res: Response) => {
     res.send("Welcome to Note App")
+})
+
+
+app.post("/create-note", (req: Request, res: Response) => {
+    const myNote = new noteModel({
+        title: "My Title",
+        body: "Here is the body of my first body",
+        isActive: true,
+        numberOfChar: 679,
+        date: Date.now,
+        comments: [
+            {
+                title: "Comment title",
+                body: "body of my first comment",
+            }
+        ]
+    })
+
+    res.status(201).json({
+        success:true,
+        message:"Note created successfully",
+        data:myNote,
+    })
 })
 
 export default app
