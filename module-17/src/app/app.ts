2268410505d2978a92661a7f32ccd1c5e8908663
Rasel Shikder <rasel.sikder777.rk@gmail.com>
2025-06-16 15:07:58 +0000

    added module-17 - 4.0

--- module-17/src/app/app.ts
+++ module-17/src/app/app.ts
@@ -1,12 +1,14 @@
 
 import express, { Application, Request, Response } from "express";
 import myNote from "./controller/noteController";
+import users from "./controller/userController";
+
 
 const app: Application = express()
 
 app.use(express.json())
 app.use("/notes", myNote)
-
+app.use("/users", users)
 
 
 app.get("/", (req: Request, res: Response) => {
