0fe3ebb852eebb930b1e35bcd7f5787ec725af3e
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2025-06-29 03:40:34 +0600

    added module-02 - 1.0

---
+++ module-22/src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button"
+
+import './App.css'
+
+function App() {
+  console.log(1+1)
+  return (
+    <>
+     <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+      <p>Simple</p>
+    </>
+  )
+}
+
+export default App
