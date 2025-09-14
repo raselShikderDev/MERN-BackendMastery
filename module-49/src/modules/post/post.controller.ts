a8979131d9423501cdd0a87f1b787ef4c48c586a
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2025-09-15 02:02:04 +0600

    added paggination, searching, filtering, sorting

--- module-49/src/modules/post/post.controller.ts
+++ module-49/src/modules/post/post.controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import { postService } from "./post.service";
 
-
 // Create a user
 const createPost = async (req: Request, res: Response) => {
   try {
@@ -17,8 +16,21 @@ const createPost = async (req: Request, res: Response) => {
 
 // Get all  user
 const getAllPost = async (req: Request, res: Response) => {
+
+  
+  const isFeatured = req.query.isFeatured ? req.query.isFeatured === "true" : undefined;
+  const author = req.query.author as string || "";
+  const search = req.query.search as string || "";
+  const sortBy = req.query.sortBy as string || "";
+  const sortOrder = (req.query.sortOrder as string).toLowerCase() === "desc" ? "desc" : "asc"
+  const tags = req.query.tags ? (req.query.tags as string).split(",") : [];
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  console.log("sortBy: ", sortBy);
+  console.log("sortOrder: ", sortOrder);
+
   try {
-    const result = await postService.getAllPost();
+    const result = await postService.getAllPost({limit, page, search, isFeatured, tags, author, sortBy, sortOrder});
     console.log("In controller get all posts: ", result);
 
     res.status(200).json(result);
@@ -44,7 +56,10 @@ const getPost = async (req: Request, res: Response) => {
 // Update user
 const updatePost = async (req: Request, res: Response) => {
   try {
-    const result = await postService.updatePost(Number(req.params.id), req.body);
+    const result = await postService.updatePost(
+      Number(req.params.id),
+      req.body
+    );
     console.log("In controller update a post: ", result);
 
     res.status(200).json(result);
@@ -67,12 +82,10 @@ const deletePost = async (req: Request, res: Response) => {
   }
 };
 
-
-
 export const postController = {
   createPost,
   getAllPost,
   getPost,
   updatePost,
-  deletePost
+  deletePost,
 };
--- module-49/src/modules/post/post.service.ts
+++ module-49/src/modules/post/post.service.ts
@@ -1,75 +1,111 @@
 import { Post, Prisma } from "@prisma/client";
 import { prisma } from "../../config/db";
 
-
-const createPost = async (payload:Prisma.PostCreateInput):Promise<Post> => {
-    const result = await prisma.post.create({
-        data:payload,
-        include:{
-          author:{
-            select:{
-              id:true,
-              name:true,
-              email:true
-            }
-          }
-        }
-    })
+const createPost = async (payload: Prisma.PostCreateInput): Promise<Post> => {
+  const result = await prisma.post.create({
+    data: payload,
+    include: {
+      author: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+        },
+      },
+    },
+  });
   return result;
 };
 
-
 // Get all user
-const getAllPost = async () => {
+const getAllPost = async ({
+  limit = 10,
+  page = 1,
+  search,
+  isFeatured,
+  tags,
+  author,
+  sortBy,
+  sortOrder,
+}: {
+  limit?: number;
+  page?: number;
+  search?: string;
+  isFeatured?: boolean;
+  tags?: string[];
+  author?: string;
+  sortBy?: string;
+  sortOrder?: "asc" | "desc";
+}) => {
+  const where: any = {
+    AND: [
+      search && {
+        OR: [
+          {title: { contains: search, mode: "insensitive" }},
+          {content: { contains: search, mode: "insensitive" }}
+        ],
+      },
+      typeof isFeatured === "boolean" && { isFeatured },
+      tags && tags.length > 0 && { tags: { hasEvery: tags } },
+      author && { author },
+    ].filter(Boolean),
+  };
+  const skip = (page - 1) * limit;
   const allPost = await prisma.post.findMany({
-    orderBy:{
-      createdAt:"asc"
-    }
-  })
-  return allPost
+    where: where,
+    take: limit,
+    skip,
+    orderBy: {
+      [sortBy || "createdAt"]: sortOrder || "desc",
+    },
+  });
+  const total = await prisma.post.count({ where });
+  return {
+    data: allPost,
+    paggination: {
+      page,
+      limit,
+      total,
+      totalpages: Math.ceil(total / limit),
+    },
+  };
 };
-
-
 
 // Update singel user  by id
-const getPost = async (id:number) => {
+const getPost = async (id: number) => {
   const post = await prisma.post.findUnique({
-    where:{
-      id
-    }
-  })
-  return post
-};
-
-
-
-
-// Get singel user  by id
-const updatePost = async (id:number, payload:Partial<Post>) => {
-   const post = await prisma.post.update({
-    where:{
-      id
+    where: {
+      id,
     },
-    data:payload
-  })
-  return post
+  });
+  return post;
 };
 
 // Get singel user  by id
-const deletePost = async (id:number) => {
-const post = await prisma.post.delete({
-    where:{
-      id
-    }
-  })
-  return post
+const updatePost = async (id: number, payload: Partial<Post>) => {
+  const post = await prisma.post.update({
+    where: {
+      id,
+    },
+    data: payload,
+  });
+  return post;
 };
 
+// Get singel user  by id
+const deletePost = async (id: number) => {
+  const post = await prisma.post.delete({
+    where: {
+      id,
+    },
+  });
+  return post;
+};
 
 export const postService = {
   createPost,
   getAllPost,
   getPost,
   updatePost,
-  deletePost
+  deletePost,
 };
