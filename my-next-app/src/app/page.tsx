980c490f1a2fcadc6a0870b623ad8c31289706a4
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2025-09-19 21:06:56 +0600

    Update my-next-app before switching to main

--- my-next-app/src/app/page.tsx
+++ my-next-app/src/app/page.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link";
 
 export default function Home() {
   console.log("Hello world");
   
   return (
-    <div className=""> Hello next.js</div>
+    <div className=""> 
+    <h1>Hello next.js</h1>
+    <Link href={"/about"}>About</Link>
+    </div>
+    
   );
 }
